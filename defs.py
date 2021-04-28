c5e8bd7566e9343afd4e2d26d3d686e02a1cea76
Gustavo Oliveira <gustavoeso@al.insper.edu.br>
2021-04-28 10:45:48 -0300

    adicionando funções para biblioteca de cartas

--- defs.py
+++ defs.py
@@ -1,3 +1,21 @@
 import math
 import random
 
+def cria_baralho():
+    baralho = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    naipes = ['♠', '♣', '♥', '♦']
+    baralho_cheio = []
+    for naipe in range(4):
+        for numero in range(13):
+            carta = (baralho[numero] + naipes[naipe])
+            baralho_cheio.append(carta)
+    return baralho_cheio
+
+def extrai_naipe(carta):
+    return carta[-1]
+
+def extrai_valor(valor):
+    if valor == '10♣' or valor == '10♥' or valor == '10♦' or valor == '10♠':
+        return valor[0:2]
+    else:
+        return valor[0]
