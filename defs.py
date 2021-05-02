d86cd0f9ed29b92ab6d3b94b88a8e2f2cf60289f
Gustavo Oliveira <gustavoeso@al.insper.edu.br>
2021-05-01 21:54:50 -0300

    adicionando movimentos para o jogo

--- defs.py
+++ defs.py
@@ -19,3 +19,64 @@ def extrai_valor(valor):
         return valor[0:2]
     else:
         return valor[0]
+
+
+def extrai_valor(valor):
+    if valor == '10♣' or valor == '10♥' or valor == '10♦' or valor == '10♠':
+        return valor[0:2]
+    else:
+        return valor[0]
+
+def extrai_naipe(carta):
+    return carta[-1]
+
+def extrai_valor(valor):
+    if valor == '10♣' or valor == '10♥' or valor == '10♦' or valor == '10♠':
+        return valor[0:2]
+    else:
+        return valor[0]
+
+def extrai_naipe(carta):
+    return carta[-1]
+
+def extrai_valor(valor):
+    if valor == '10♣' or valor == '10♥' or valor == '10♦' or valor == '10♠':
+        return valor[0:2]
+    else:
+        return valor[0]
+
+def extrai_naipe(carta):
+    return carta[-1]
+
+def lista_movimentos_possiveis(baralho, indice):
+    lista = []
+    
+    valor = extrai_valor(baralho[indice])
+    valor_anterior = extrai_valor(baralho[indice-1])
+
+    naipe = extrai_naipe(baralho[indice])
+    naipe_anterior = extrai_naipe(baralho[indice-1])
+
+    if indice == 0:
+        return lista
+
+    if indice <= 2:
+        if valor == valor_anterior or naipe == naipe_anterior:
+            lista += [1]
+            return lista
+        else:
+            return lista
+    if indice >= 3:
+        valor_3_anterior = extrai_valor(baralho[indice-3])
+        naipe_3_anterior = extrai_naipe(baralho[indice-3])
+
+        if (valor == valor_anterior or naipe == naipe_anterior) and (valor == valor_3_anterior or naipe == naipe_3_anterior):
+            lista += [1, 3]
+            return lista
+        if valor == valor_anterior or naipe == naipe_anterior:
+            lista += [1]
+        if valor == valor_3_anterior or naipe == naipe_3_anterior:
+            lista += [3]
+            return lista
+        else:
+            return lista
